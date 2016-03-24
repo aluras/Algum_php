21e4b12aea4dd54cacdf60b346e1f0ed4636d31d
André Luis <andrelrs80@gmail.com>
2016-03-24 13:23:46 -0300

    Finalização da autenticação google

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sn1007071
+ * Date: 23/03/2016
+ * Time: 10:13
+ */
+
+class UsersController extends AppController {
+    public function opauth_complete() {
+        debug($this->data);
+    }
+}
